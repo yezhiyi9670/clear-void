dc97fb29d6eb447332517a4d68074b7c53af000e
yezhiyi9670 <yezhiyi0562@foxmail.com>
2025-11-02 14:41:07 +0800

    Initial template commit

---
+++ common/src/main/java/yezhiyi9670/clearvoid/ExampleMod.java
@@ -0,0 +1,9 @@
+package yezhiyi9670.clearvoid;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "clearvoid";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/yezhiyi9670/clearvoid/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package yezhiyi9670.clearvoid.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import yezhiyi9670.clearvoid.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
